3cfadfce922aa86d7a33054e93357558f531d457
Nodar <nodar.panchenko@gmail.com>
2023-07-19 21:46:00 +0400

    Изменил способ получения вакансий через api по заданным работадателям.

--- src/api.py
+++ src/api.py
@@ -3,16 +3,14 @@ import requests
 
 class HeadHunter:
     api = 'https://api.hh.ru/vacancies'
+    employers = ('1740', '78638',
+                 '3009025', '1868342',
+                 '652744', '9739229',
+                 '5714322', '9632510',
+                 '83554694', '9288781')
 
-    def __init__(self, job_name=None):
-        self.vacancies = None
-        self.job_name = job_name
-
-    def get_vacancies(self):
-        response = requests.get(HeadHunter.api, params={'text': self.job_name, 'employer_id': {'1740', '78638',
-                                                                                               '3009025', '1868342',
-                                                                                               '652744', '9739229',
-                                                                                               '5714322', '9632510',
-                                                                                               '83554694', '9288781'}})
-        self.vacancies = response.json()['items']
-        return self.vacancies
+    @classmethod
+    def get_vacancies(cls, employer):
+        response = requests.get(HeadHunter.api, params={'employer_id': employer})
+        vacancies = response.json()['items']
+        return vacancies
